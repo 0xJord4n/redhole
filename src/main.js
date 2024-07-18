d0d729f21626be0103446ff27c7a95971ad919a7
Lirone <lirone88@outlook.fr>
2024-07-18 15:38:47 +0200

    add: handle students who got blackholed

--- src/main.js
+++ src/main.js
@@ -23,24 +23,48 @@ document.onreadystatechange = async () => {
 
   const cursusBeginDate = new Date(profile.cursus_begin_date);
 
+  console.log("[REDHOLE] cursusBeginDate:", cursusBeginDate.getDate());
+
   const blackholeDate = new Date(cursusBeginDate);
   blackholeDate.setDate(
     cursusBeginDate.getDate() +
       BLACKHOLE_PER_MILESTONE_FROM_START[profile.milestone]
   );
 
+  const todayDate = new Date();
+  const deadlineDate = new Date(profile.deadline);
+
+  if (todayDate > deadlineDate) {
+    console.log("[REDHOLE] the deadline has passed");
+    isDeadlineExceeded = true;
+  } else {
+    console.log("[REDHOLE] the deadline has not passed.");
+    isDeadlineExceeded = false;
+  }
+
   const textSelector = document.querySelector(
     "#root > div > div.content.md\\:pl-20.pt-16.content-animation > div:nth-child(1) > header > div > div.border.border-neutral-600.bg-ft-gray\\/50.rounded-xl.relative.flex.flex-col.w-full.sm\\:gap-3.justify-between > div.pl-7.pt-8.flex.flex-col.lg\\:flex-row > div.lg\\:pl-2.overflow-hidden > h2.text-2xl.font-bold.text-center.py-4.lg\\:text-left.lg\\:py-0.drop-shadow-md"
   );
 
-  textSelector.insertAdjacentHTML(
-    "afterend",
+  if (isDeadlineExceeded)
+  {
+    textSelector.insertAdjacentHTML(
+      "afterend",
+      `
+    <h2 class="text-xl font-bold text-center py-4 lg:text-left lg:py-0 drop-shadow-md text-destructive">Been absorbed by the BlackHole</h2>
     `
-	<h2 class="text-xl font-bold text-center py-4 lg:text-left lg:py-0 drop-shadow-md text-destructive">Blackhole on ${
-    blackholeDate.toLocaleString().split(",")[0]
-  }</h2>
-	`
-  );
-
-  console.log("[REDHOLE] Blackhole date:", blackholeDate);
+    );
+  }
+  else
+  {
+    textSelector.insertAdjacentHTML(
+      "afterend",
+      `
+    <h2 class="text-xl font-bold text-center py-4 lg:text-left lg:py-0 drop-shadow-md text-destructive">Blackhole on ${
+      blackholeDate.toLocaleString().split(",")[0]
+    }</h2>
+    `
+    );
+    console.log("[REDHOLE] Blackhole date:", blackholeDate);
+  }
 };
